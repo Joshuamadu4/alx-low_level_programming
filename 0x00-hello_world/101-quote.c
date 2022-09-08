89d7b9f0d94b2f5a1b29ae91d9eaac042e52e6bf
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-08 13:04:25 -0700

    Great One

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+/**
+ *
+ * main - prints out the last part of a quote in the standard error.
+ * Return: 1 if success. 
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	return (1);
+}
