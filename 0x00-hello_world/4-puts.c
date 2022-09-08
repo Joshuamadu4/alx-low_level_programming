a1e8e2e3827f253a4005b2b2c4278e5dac37e5ba
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-08 07:51:37 -0700

    New Correction Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- *
  * main - Entry point
- *
- *
  *
  * Return: 0 after printing the function
  */
-
 int main(void)
 
 {puts("\"Programming is like building a multilingual puzzle");
