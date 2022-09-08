5c8bb44d99f3865d368423ff3e6343a851e251bb
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-08 07:42:37 -0700

    Corrected scripts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,23 +1,16 @@
 #include <stdio.h>
 
-
-
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ * Return: 0 after printing the function
+ */
 
 int main(void)
 
-{
-
-		puts("\"Programming is like building a multilingual puzzle");
-
-			return (0);
-
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
