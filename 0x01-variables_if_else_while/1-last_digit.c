fa498950ba475dae8006c8407931ec8a28cfda66
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-09 13:49:31 -0700

    Low level programming

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,7 +23,7 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, lastd);
 	}
-	else if (lastd < 6 && lastd !=0)
+	else if (lastd < 6 && lastd != 0)
 	{
 		printf("Last digit of %d is %d and is less than  6 and not 0\n", n, lastd);
 	}
