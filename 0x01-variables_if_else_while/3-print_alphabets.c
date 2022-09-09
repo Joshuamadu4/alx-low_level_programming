c1a84fefa32bfcae8638fad79bd362324d423136
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-09 16:13:54 -0700

    Corrected Capital and small letters

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char x;
 
-	for (x = 'A'; x <= 'Z'; x++)
+	for (x = 'a'; x <= 'z'; x++)
 	{
 		putchar(x);
 	}
 
-	for (x = 'a'; x <= 'z'; x++)
+	for (x = 'A'; x <= 'Z'; x++)
 	{
 		putchar(x);
 	}
