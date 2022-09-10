447f4309e9f68c0b16d5566b475b1657cba48722
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-10 04:19:17 -0700

    Great Correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -21,18 +21,28 @@ int main(void)
 			{
 				for (d = 48; d <= 57; ++d)
 				{
+					if (((c + d) > (a + b) && c >= a) || a < c)
 					putchar(a);
 					putchar(b);
+					putchar(' ');
 					putchar(c);
 					putchar(d);
-					if (a != 57 || b != 56 || c != 57 || d != 57)
+
+					if (a + b + c + d == 227 && c == 57)
 					{
-						putchar(44);
-						putchar(32);
+					break;
+					}
+					else
+					{
+					putchar(',');
+					putchar(' ');
 					}
 				}
 			}
 		}
 	}
+
+	putchar('\n');
+
 	return (0);
 }
