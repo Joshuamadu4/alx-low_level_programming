1c8478c0ca5138bcaadc3a5ef17006364c95c79c
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-10 04:38:38 -0700

    New Variabele correction

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,13 +13,13 @@ int main(void)
 	int c = 48;
 	int d = 48;
 
-	for (a = 48; a <= 57; ++a)
+	for (a = 48; a <= 57; a++)
 	{
-		for (b = 48; b <= 57; ++b)
+		for (b = 48; b <= 57; b++)
 		{
-			for (c = 48; c <= 57; ++c)
+			for (c = 48; c <= 57; c++)
 			{
-				for (d = 48; d <= 57; ++d)
+				for (d = 48; d <= 57; d++)
 				{
 					if (((c + d) > (a + b) && c >= a) || a < c)
 					putchar(a);
