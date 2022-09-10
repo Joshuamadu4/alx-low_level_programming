a78cba8aaecda4c2b54cb1ecac20ff2dc7d30b15
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-10 03:33:27 -0700

    Great work done

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Prints a text according number
- * 
+ *
  * Return: Always (Success)
  */
 int main(void)
@@ -13,7 +13,6 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 
 	if ((n % 10) > 5)
 	{
