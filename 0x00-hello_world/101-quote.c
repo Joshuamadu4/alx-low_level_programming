e9d6affbf03ba6d162bd6833ba96840f5f015ce7
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-12 04:03:35 -0700

    Writing Sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,10 @@
 
 /**
  * main - prints out the last part of a quote in the standard error.
- * Return: 1 if success.
+ * Return: 1 if success
  */
 int main(void)
 {
-	fwrite (2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
 	return (1);
 }
