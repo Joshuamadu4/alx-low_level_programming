d5a8522a4a24285b52f40593b445684967759c46
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-13 16:18:47 -0700

    Printing Alphabetical character

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Printing Alphabets
+ * return: 0 Success
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c); 
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,14 @@
 int _putchar (char a);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int a);
+int _isalpha(int a);
+int print_sign(int n); 
+int _abs(int n); 
+int print_last_digit(int);
+void jack _bauer(void);
+void times_table(void);
+int add(int, int);
+void print_to_98(int n);
+void print_times_table(int n);
+
