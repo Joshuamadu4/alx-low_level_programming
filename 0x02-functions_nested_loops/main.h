5e19cd8a1bb6032728e1ab78dd66f4f81569cc08
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 04:21:17 -0700

    Prints Last Number

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,7 +11,7 @@ int print_last_digit(int);
 void jack_bauer(void);
 void times_table(void);
 int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+void print_to_98(int a);
+void print_times_table(int a);
 
 #endif
