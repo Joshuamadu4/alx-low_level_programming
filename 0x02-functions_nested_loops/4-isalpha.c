e54b6efe942a2cb65c7608ede220f79142566f77
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 03:42:37 -0700

    Displaying AlphabetsCapital and Small

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,7 +3,7 @@
 /**
  * _isalpha - function to check if c is  a letter, lowercase or uppercase
  * @a: is the int that will use for the argument of the function
- * Return: 0 (Success) 
+ * Return: 0 (Success)
  */
 
 int _isalpha(int a)
