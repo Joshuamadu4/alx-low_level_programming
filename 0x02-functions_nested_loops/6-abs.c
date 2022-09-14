27bef548b30095f5b299132231a4723587ace75d
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 04:13:43 -0700

    Absolute value of numbers

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 
 /**
  * _abs - function that computes the absolute value of an integer
- * @c:  is the int that will use for the argument of the function
+ * @a:  is the int that will use for the argument of the function
  *
  * Return: 0
  */
