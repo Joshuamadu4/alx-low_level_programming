4be681a3ecd34c12917045d1fe5828ceb5915804
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 01:19:29 -0700

    Corrected

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isalpha(int a);
 int print_sign(int n); 
 int _abs(int n); 
 int print_last_digit(int);
-void jack _bauer(void);
+void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
