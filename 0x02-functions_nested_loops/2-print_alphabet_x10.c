9bfc1b10b2c5fe6110be907f5a194262b954cb2c
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 02:37:12 -0700

    Printing Alphabetical character

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * Print_alphabet_x10 - Printing Alphabets 10 times 
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	char a, i; 
+	
+	for (i =0; i <= '2'; i++)
+	{
+		for (a = 'a'; a <= '2'; a++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n);
+	}
+}
