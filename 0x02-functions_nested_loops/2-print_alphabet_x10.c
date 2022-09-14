56ecfc92e0d81f59ecb999e99f27a44bd87bc6dc
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-14 02:46:15 -0700

    Printing Alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,9 +9,9 @@ void print_alphabet_x10(void)
 {
 	char a, i; 
 	
-	for (i =0; i <= '2'; i++)
+	for (i = 0; i <= '9'; i++)
 	{
-		for (a = 'a'; a <= '2'; a++)
+		for (a = 'a'; a <= 'z'; a++)
 		{
 			_putchar(a);
 		}
