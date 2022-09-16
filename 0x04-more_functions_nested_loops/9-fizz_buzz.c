530be3042ff1cb8ce070478610092b98249c935c
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-16 13:41:57 -0700

    New update

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,7 +11,7 @@ int main(void)
 	while (h <= 100)
 	{
 		if (h % 3 == 0 && h % 5 == 0)
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		else if (h % 5 == 0)
 		{
 			if (h == 100)
@@ -20,13 +20,13 @@ int main(void)
 				printf("\n");
 			}
 			else
-				printf("Buzz ");
+				printf("Buzz");
 		}
 		else if (h % 3 == 0)
-			printf("Fizz ");
+			printf("Fizz");
 		else
 			printf("%d ", h);
 	h++;
 	}
-	Return (0)
+	return (0);
 }
