d57cbf29f9b16e6ce540f6160de861a893c98953
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-16 13:30:46 -0700

    Corrected work

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,45 +1,32 @@
 #include <stdio.h>
 
 /**
- * main - program that prints either number
- *
- * or fizz or buzz or fizzBuzz
- *
- * Return: returns 0
+ * main - function that checks for uppercase character.
+ * Return: 0
  */
-
 int main(void)
-
 {
-	int num = 1;
+	int h = 1;
 
-	while (num++ < 100)
+	while (h <= 100)
 	{
-		if ((num % 3 == 0) && (num % 5 == 0))
-		{
+		if (h % 3 == 0 && h % 5 == 0)
 			printf("FizzBuzz ");
-		}
-		else if ((num % 3) == 0)
+		else if (h % 5 == 0)
 		{
-			printf("Fizz ");
-		}
-		else if ((num % 5) == 0)
-		{
-			if (num != 100)
-			{
-				printf("Buzz ");
-			}
-			else
+			if (h == 100)
 			{
 				printf("Buzz");
+				printf("\n");
 			}
+			else
+				printf("Buzz ");
 		}
+		else if (h % 3 == 0)
+			printf("Fizz ");
 		else
-		{
-			printf("%d ", num);
-		}
+			printf("%d ", h);
+	h++;
 	}
-	printf("\n");
-
-	return (0);
+	Return (0)
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,18 +1,5 @@
 #ifndef MAIN_H
-
 #define MAIN_H
-
-
-
-/**
- *
- * *main - header file for protoypes
- *
- * *void protoypes (void)
- *
- * *int protoypes (int)
- *
- * */
 
 
 
