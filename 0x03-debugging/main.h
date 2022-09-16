45a45c66ac534da786003a8afdf882cd7dedb763
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-16 05:11:30 -0700

    Correction main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,7 +6,8 @@
 
 void positive_or_negative(int i);
 int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
+int convert_day(int month, int day)
+void print_remaining_days(int month, int day, int year);
 
 
 #endif /* MAIN_H */
