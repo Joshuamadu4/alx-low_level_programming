90c25edbb9c4afc1eaf2597d2e5e385d841cd119
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-19 18:09:40 -0700

    Corrected work

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,17 +10,22 @@
 
 void rev_string(char *s)
 {
-	int len = 0, swap = 0, i = 0, fn = 0;
+	int i, j, k, temp;
 
-	len = _strlen(s);
-	if (len > 0)
+	i = 0;
+	while (s[i] != '\0')
 	{
-		for (i = 0; i < ((len) / 2); i++)
-		{
-			swap = *(s + i);
-			fn = (len - 1) - i;
-			*(s + i) = *(s + fn);
-			*(s + fn) = swap;
-		}
+		i++;
+	}
+	k = 0;
+	j = i - 1;
+
+	while (k < j)
+	{
+		temp = s[k];
+		s[k] = s[j];
+		s[j] = temp;
+		k++;
+		j--;
 	}
 }
