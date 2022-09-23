7047d5917806e89866bd376c87bc4c5e290fc239
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-23 09:52:42 -0700

    Task 101

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,32 +1,19 @@
-#ifndef MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
-#define MAIN_H
 
-#include <stdio.h>
 
-/**
- *
- *  *void prototypes()
- *
- *   *int prototypes()
- *
- *    *char prototypes()
- *
- *     */
-
-void reverse_array(int *a, int n);
-
-void print_number(int n);
-
-void print_buffer(char *b, int size);
-
-int _strcmp(char *s1, char *s2);
+int _putchar(char c);
 
 char *_strcat(char *dest, char *src);
 
 char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
+
+int _strcmp(char *s1, char *s2);
+
+void reverse_array(int *a, int n);
 
 char *string_toupper(char *);
 
@@ -36,10 +23,12 @@ char *leet(char *);
 
 char *rot13(char *);
 
+void print_number(int n);
+
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
-char *add_strings(char *n1, char *n2, char *r, int r_index);
+void print_buffer(char *b, int size);
 
 
 
-#endif /*MAIN_H*/
+#endif
