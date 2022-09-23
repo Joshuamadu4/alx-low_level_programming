daddcdc73994e7390b67374e9a4e561a5fd7e7e3
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-23 09:34:20 -0700

    Task 101

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -12,15 +12,19 @@
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int i, j, k, l, m, n;
-	
+
 	for (i = 0; n1[i]; i++);
+
 	for (j = 0; n2[j]; j++);
+
 	if (i > size_r || j > size_r)
+
 		return (0);
 	m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
+
 		if (i >= 0)
 			n += n1[i] - '0';
 		if (j >= 0)
@@ -35,7 +39,7 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
-	
+
 	for (k -= 1, l = 0; l < k; k--, l++)
 	{
 		m = r[k];
