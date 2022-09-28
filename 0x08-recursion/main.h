1998bce08e56758531412ea7ab10a364ff76748e
joshuamadu4 <joshuamadu4@gmail.com>
2022-09-28 08:38:20 -0700

    Palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,7 @@ int _strlen_recursion(char *s);
 int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
-int _putchar(char c); 
+int _putchar(char c);
+int is_palindrome(char *s);
+int wildcmp(char *s1, char *s2);
 #endif
