de23f88a339853d591a81d221be93c2f16edd65b
joshuamadu4 <joshuamadu4@gmail.com>
2022-10-01 16:24:52 -0700

    Task 0 corrected

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,8 +1,8 @@
 #include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout   
- *@c: The character to print 
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
  * Return: on success 1.
  * on error, -1 is returned, and errno is set appropriately.
