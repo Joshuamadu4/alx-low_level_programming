8bcee684f450e28b9586dd8846d47fedd3edacaa
joshuamadu4 <joshuamadu4@gmail.com>
2022-10-06 19:09:24 -0700

    Task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,6 +17,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	unsigned int len = n, index;
 
 	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
 		s2 = "";
 
 	for (index = 0; s1[index]; index++)
