3121d1584ed46ea6d6487d27a70978e0a2c611ac
joshuamadu4 <joshuamadu4@gmail.com>
2022-10-20 08:35:16 -0700

    Task 101

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .text
+global main
+
+main:
+mov rax, 1
+
+	mov rdi, 1
+	mov rsi, msg
+	mov rdx, msglen
+	syscall
+
+
+	mov rax, 60
+	mov rdi, 0
+	syscall
+
+
+section .rodata
+	msg: db "Hello, Holberton", 10
+	msglen: equ $ - msg
