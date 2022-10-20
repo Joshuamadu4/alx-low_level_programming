11592147bb98a5302bcdadc7b81ac79b674cf5f3
joshuamadu4 <joshuamadu4@gmail.com>
2022-10-20 08:43:41 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,19 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
 			printf("[%d] %s\n", h->len, h->str);
 
+		nodes++;
 		h = h->next;
-		count++;
 	}
-	return (count);
+
+	return (nodes);
 }
