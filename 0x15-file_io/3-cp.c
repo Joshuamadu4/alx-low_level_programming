5b7114590a302c558f4e83a88e0b9756c624ae1f
joshuamadu4 <joshuamadu4@gmail.com>
2022-10-31 09:58:57 -0700

    Corrected work

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,11 +2,11 @@
 #include "main.h"
 
 /**
- * main - program that copies the content of a file to another file
+ * error_file - checks if files can be opened.
  * @file_from: file_from.
  * @file_to: file_to.
  * @argv: arguments vector.
- * 
+ *
  * Return: no return.
  */
 void error_file(int file_from, int file_to, char *argv[])
